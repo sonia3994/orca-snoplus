e447cb9e95db1ef1618da5b8e95aa8d9fa4908f7
Chris Jones <christopher.jones@physics.ox.ac.uk>
2014-11-14 16:54:27 +0000

    developing changes to the runtype

--- Source/Experiments/SNOP/SNOPModel.h
+++ Source/Experiments/SNOP/SNOPModel.h
@@ -94,9 +94,11 @@
     
     bool _smellieDBReadInProgress;
     bool _smellieDocUploaded;
+    unsigned long snopRunTypeMask;
 }
 
 @property (nonatomic,retain) NSMutableDictionary* smellieRunHeaderDocList;
+@property (nonatomic,assign) unsigned long snopRunTypeMask;
 
 @property (nonatomic,copy) NSString* orcaDBUserName;
 @property (nonatomic,copy) NSString* orcaDBPassword;
@@ -187,8 +189,8 @@
 - (ORCouchDB*) orcaDbRefWithEntryDB:(id)aCouchDelegate withDB:(NSString*)entryDB;
 
 //run type definition functions
-- (void) setRunType:(int)aRunType;
-- (int) getRunType;
+- (void) setSnopRunTypeMask:(unsigned long)aRunType;
+- (unsigned long) getSnopRunTypeMask;
 
 //smellie functions -------
 - (void) getSmellieRunListInfo;
